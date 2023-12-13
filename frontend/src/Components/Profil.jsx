e0cd2d813de58a27d9fbde55cc9859e22c05592d
Guillaume <guizanko@hotmail.fr>
2023-12-13 15:54:35 +0100

    editbutton

--- frontend/src/Components/Profil.jsx
+++ frontend/src/Components/Profil.jsx
@@ -6,9 +6,35 @@ import { FaRegEdit } from "react-icons/fa";
 
 function Profile() {
   const [isOpen, setClose] = useState(false);
+  const [isEditing, setEditing] = useState(false);
+
   const toggleNavLeft = () => {
     setClose(!isOpen);
   };
+
+  const [profileInfo, setProfileInfo] = useState({
+    fullName: "Blablabla",
+    emailAddress: "blablabla@example.com",
+    phoneNumber: "+32474025647",
+    address: "123 rue Saint Antoine",
+  });
+
+  const updateProfileInfo = (fieldName, newValue) => {
+    setProfileInfo((prevProfileInfo) => ({
+      ...prevProfileInfo,
+      [fieldName]: newValue,
+    }));
+  };
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    setEditing(false);
+    // Ajoutez ici la logique pour sauvegarder les modifications, si nécessaire
+  };
+
   return (
     <div className="containermain w-screen flex flex-col mt-5 overflow-hidden">
       <div className="flex h-[10vh] w-screen flex-row-reverse w-screen relative items-center md:justify-between justify-around">
@@ -30,45 +56,118 @@ function Profile() {
         className={`lg:absolute lg:left-[20vw] lg:bottom-0 lg:w-[80vw] lg:bg-white/10 h-[85vh] text-white text-3xl flex items-center justify-center ${
           isOpen ? "bg-white/0.5" : ""
         }`}
+      ></section>
+      <section
+        className={`lg:absolute lg:left-[20vw] lg:bottom-0 lg:w-[80vw] lg:bg-white/10 h-[85vh] text-white text-3xl flex items-center justify-center ${
+          isOpen ? "bg-white/0.5" : ""
+        }`}
       >
-        <div class="px-6 py-6 lglass rounded-3xl items-center justify-center">
-          <h3 class="w-full flex justify-center text-[#b4d429] text-3xl font-bold">
+        <div className="px-6 py-6 lglass rounded-3xl items-center justify-center">
+          <h3 className="w-full flex justify-center text-[#b4d429] text-3xl font-bold">
             User Profile
           </h3>
-          <div class="items-center justify-center text-center mt-2">
-            <dl class="sm:w-fit lg:w-fit">
-              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Full name</dt>
-                <dd class="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
-                  Blablabla
+          <div className="items-center justify-center text-center mt-2">
+            {isEditing ? (
+              <form>
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    value={profileInfo.fullName}
+                    onChange={(e) => updateProfileInfo("fullName", e.target.value)}
+                    className="border rounded p-1 text-gray-300"
+                  />
                 </dd>
               </div>
-              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">
-                  Email address
-                </dt>
-                <dd class="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
-                  Blablabla
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="email"
+                    value={profileInfo.emailAddress}
+                    onChange={(e) => updateProfileInfo("emailAddress", e.target.value)}
+                    className="border rounded p-1 text-gray-300"
+                  />
                 </dd>
               </div>
-              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Phone number</dt>
-                <dd class="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
-                  Blablabla
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Phone number</dt>
+                <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="tel"
+                    value={profileInfo.phoneNumber}
+                    onChange={(e) => updateProfileInfo("phoneNumber", e.target.value)}
+                    className="border rounded p-1 text-gray-300"
+                  />
                 </dd>
               </div>
-              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Address</dt>
-                <dd class="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
-                  Blablabla
+              <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Address</dt>
+                <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    value={profileInfo.address}
+                    onChange={(e) => updateProfileInfo("address", e.target.value)}
+                    className="border rounded p-1 text-gray-300"
+                  />
                 </dd>
               </div>
-            </dl>
+              {/* Ajoutez d'autres champs éditables similaires ici */}
+            </form>
+            
+            
+            ) : (
+              <dl className="sm:w-fit lg:w-fit">
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt>
+                  <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                    {profileInfo.fullName}
+                  </dd>
+                </div>
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Email address
+                  </dt>
+                  <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                    {profileInfo.emailAddress}
+                  </dd>
+                </div>
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Phone number
+                  </dt>
+                  <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                    {profileInfo.phoneNumber}
+                  </dd>
+                </div>
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">Address</dt>
+                  <dd className="mt-1 text-sm text-white-900 sm:mt-0 sm:col-span-2">
+                    {profileInfo.address}
+                  </dd>
+                </div>
+              </dl>
+            )}
           </div>
+          {isEditing ? (
+            <button
+              className="fancy w-1/2 mt-2 md:w-60 md:mb-4 w-40 mx-auto flex items-center justify-center text-center text-xl md:text-xl bg-[#60701a] py-2 rounded-md text-[#b4d429] border border-[#b4d429] hover:scale-105 group-hover:animate-shine focus:ring"
+              onClick={handleSaveClick}
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              className="fancy w-1/2 mt-2 md:w-60 md:mb-4 w-40 mx-auto flex items-center justify-center text-center text-xl md:text-xl bg-[#60701a] py-2 rounded-md text-[#b4d429] border border-[#b4d429] hover:scale-105 group-hover:animate-shine focus:ring"
+              onClick={handleEditClick}
+            >
+              Edit Profile
+            </button>
+          )}
         </div>
-        <button className="absolute top-0 right-0 bg-[#b4d429] text-white px-4 py-2 rounded-3xl mt-4">
-          Edit Profile
-        </button>
       </section>
     </div>
   );
